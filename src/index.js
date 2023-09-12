d15fb5222f291524741f254fb7414de58a142388
Santiago Rodriguez <svntiiago@gmail.com>
2023-09-12 11:58:34 -0400

    react and github pages - no bueno

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import { HashRouter } from "react-router-dom";
+import "./index.css";
 
-ReactDOM.render(
-    <React.StrictMode>
-        <HashRouter>
-            <App />
-        </HashRouter>
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
