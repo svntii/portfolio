b385880c5877b146cfa41f67e312c4a236d1e6ac
Santiago Rodriguez <svntiiago@gmail.com>
2024-07-13 21:54:14 -0400

    added captured

--- src/components/bNav.jsx
+++ src/components/bNav.jsx
@@ -1,80 +1,85 @@
-import React, { useState, useEffect } from 'react';
-import Box from '@mui/material/Box';
-import BottomNavigation from '@mui/material/BottomNavigation';
-import BottomNavigationAction from '@mui/material/BottomNavigationAction';
-import HomeIcon from '@mui/icons-material/Home';
-import RssFeedIcon from '@mui/icons-material/RssFeed';
-import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
-import CodeIcon from '@mui/icons-material/Code';
-import Link from 'next/link';
-import GetAppIcon from '@mui/icons-material/GetApp';
-import { useRouter } from 'next/router';
-
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import HomeIcon from "@mui/icons-material/Home";
+import RssFeedIcon from "@mui/icons-material/RssFeed";
+import WorkHistoryIcon from "@mui/icons-material/WorkHistory";
+import CodeIcon from "@mui/icons-material/Code";
+import Link from "next/link";
+import GetAppIcon from "@mui/icons-material/GetApp";
+import { useRouter } from "next/router";
+import CameraIcon from "@mui/icons-material/Camera";
 
 export function BottomNav() {
-    const router = useRouter();
-    const [value, setValue] = useState(getInitialValue(router.pathname));
-    useEffect(() => {
-        setValue(getInitialValue(router.pathname));
-    }, [router.pathname]);
+  const router = useRouter();
+  const [value, setValue] = useState(getInitialValue(router.pathname));
+  useEffect(() => {
+    setValue(getInitialValue(router.pathname));
+  }, [router.pathname]);
 
-    function getInitialValue(path) {
-        switch (true) {
-            case path === '/project':
-            case path.startsWith('/project/'):
-                return 0;
-            case path === '/blog':
-            case path.startsWith('/blog/'):
-                return 1;
-            case path === '/':
-                return 2;
-            case path === '/career':
-                return 3;
-            case path === '/SJRODRIGUEZ_ATS.pdf':
-                return 4;
-            default:
-                return 2;
-        }
+  function getInitialValue(path) {
+    switch (true) {
+      case path === "/project":
+      case path.startsWith("/project/"):
+        return 0;
+      case path === "/blog":
+      case path.startsWith("/blog/"):
+        return 1;
+      case path === "/":
+        return 2;
+      case path === "/career":
+        return 3;
+      case path === "/captured":
+        return 4;
+      case path === "/SJRODRIGUEZ_ATS.pdf":
+        return 5;
+      default:
+        return 2;
     }
+  }
 
-    return (
-        <Box sx={{ width: '100%', position: 'fixed', bottom: 0 }}>
-            <BottomNavigation
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
+  return (
+    <Box sx={{ width: "100%", position: "fixed", bottom: 0 }}>
+      <BottomNavigation
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+      >
+        <BottomNavigationAction
+          icon={<CodeIcon style={{ fontSize: 40 }} />}
+          LinkComponent={Link}
+          href="/project"
+        />
 
-            >
-                <BottomNavigationAction
-                    icon={<CodeIcon style={{ fontSize: 40 }} />}
-                    LinkComponent={Link}
-                    href='/project'
-                />
-
-                <BottomNavigationAction
-                    icon={<RssFeedIcon style={{ fontSize: 40 }} />}
-                    LinkComponent={Link}
-                    href='/blog'
-                />
-                <BottomNavigationAction
-                    icon={<HomeIcon style={{ fontSize: 40 }} />}
-                    LinkComponent={Link}
-                    href='/'
-                />
-                <BottomNavigationAction
-                    icon={<WorkHistoryIcon style={{ fontSize: 40 }} />}
-                    LinkComponent={Link}
-                    href='/career'
-                />
-                <BottomNavigationAction
-                    icon={<GetAppIcon style={{ fontSize: 40 }} />}
-                    href='/SJRODRIGUEZ_ATS.pdf'
-                    download="SJRODRIGUEZ_ATS.pdf"
-                    target='_blank'
-                />
-            </BottomNavigation>
-        </Box>
-    );
-};
-
+        <BottomNavigationAction
+          icon={<RssFeedIcon style={{ fontSize: 40 }} />}
+          LinkComponent={Link}
+          href="/blog"
+        />
+        <BottomNavigationAction
+          icon={<HomeIcon style={{ fontSize: 40 }} />}
+          LinkComponent={Link}
+          href="/"
+        />
+        <BottomNavigationAction
+          icon={<WorkHistoryIcon style={{ fontSize: 40 }} />}
+          LinkComponent={Link}
+          href="/career"
+        />
+        <BottomNavigationAction
+          icon={<CameraIcon style={{ fontSize: 40 }} />}
+          LinkComponent={Link}
+          href="/captured"
+        />
+        <BottomNavigationAction
+          icon={<GetAppIcon style={{ fontSize: 40 }} />}
+          href="/SJRODRIGUEZ_ATS.pdf"
+          download="SJRODRIGUEZ_ATS.pdf"
+          target="_blank"
+        />
+      </BottomNavigation>
+    </Box>
+  );
+}
---
+++ src/pages/captured/index.js
@@ -0,0 +1,136 @@
+"use client";
+import { Box, Stack, Typography, Paper } from "@mui/material";
+import Head from "next/head";
+import { motion } from "framer-motion";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
+import "./index.css";
+
+const photosData = [
+  {
+    project: "misc",
+    date: "2024-05-18",
+    src: "/captures/misc/20240518_AlbChurch.JPG",
+    camera: "",
+    shotby: "Christian Matthew",
+    model: "",
+    alt: "Alberquerque Church",
+  },
+  {
+    project: "misc",
+    date: "2024-05-18",
+    src: "/captures/misc/20240518_standing.JPG",
+    camera: "",
+    shotby: "Christian Matthew",
+    model: "@svntiiago",
+    alt: "Standing",
+  },
+  {
+    project: "misc",
+    date: "2024-05-18",
+    src: "/captures/misc/20240518_tent.JPG",
+    camera: "",
+    shotby: "Christian Matthew",
+    model: "",
+    alt: "Tent in the morning",
+  },
+  {
+    project: "misc",
+    date: "2024-05-18",
+    src: "/captures/misc/20240518_arches.JPG",
+    camera: "",
+    shotby: "Christian Matthew",
+    model: "@svntiiago",
+    alt: "Santiago in the Distance",
+  },
+  {
+    project: "misc",
+    date: "2024-05-18",
+    src: "/captures/misc/20240518_arch.JPG",
+    camera: "",
+    shotby: "Christian Matthew",
+    model: "",
+    alt: "Arches in the sky",
+  },
+  {
+    project: "misc",
+    date: "2022-07-07",
+    src: "/captures/misc/20220707_lostPath..jpg",
+    camera: "iphone xr",
+    shotby: "Santiago Rodriguez",
+    model: "",
+    alt: "Lost Path",
+  },
+];
+
+export default function AllCaptures() {
+  const title = "captured 📸";
+
+  return (
+    <motion.Box
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Gallery />
+    </motion.Box>
+  );
+}
+
+function Gallery() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    accessibility: true,
+    arrows: true,
+  };
+
+  return (
+    <Slider {...settings}>
+      {photosData.map((photo, index) => (
+        <Paper elevation={3} sx={{ padding: 2, margin: 2 }} key={index}>
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+            spacing={3}
+          >
+            <div
+              style={{
+                position: "relative",
+                width: "100%",
+                height: "65vh",
+              }}
+            >
+              <Image src={photo.src} alt={photo.alt} fill objectFit="contain" />
+            </div>
+            <Box
+              sx={{
+                textAlign: "left",
+                background: "gray",
+                color: "white",
+                p: 1,
+              }}
+            >
+              <Typography variant="body1">project: {photo.project}</Typography>
+              <Typography variant="body1">date: {photo.date}</Typography>
+              <Typography variant="body1">camera: {photo.camera}</Typography>
+              <Typography variant="body1">shot by: {photo.shotby}</Typography>
+              <Typography variant="body1">
+                model: {photo.model || "n/a"}
+              </Typography>
+            </Box>
+          </Stack>
+        </Paper>
+      ))}
+    </Slider>
+  );
+}
